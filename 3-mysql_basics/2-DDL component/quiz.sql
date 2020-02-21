90fc4c9ff52f7c93b5757453689c0fe940aff595
AdelMahmoudHussein <adelmahmoud1991@gmail.com>
2020-02-22 01:49:02 +0200

    solve DDL quiz

--- 3-mysql_basics/2-DDL component/quiz.sql
+++ 3-mysql_basics/2-DDL component/quiz.sql
@@ -1,29 +1,50 @@
 
 # Create a new database with the name "quizzes"
-## CODE HERE
+CREATE DATABASE quizzes;
 
 # Create a new table with the name "students" into the database "quizzes" with these columns :
 # id - first_name - last_name - user_name - student_email - scores - serial_number - created
-## CODE HERE
+-- go inside database then run next code (is there any way to change current working on database by coding?)
+CREATE TABLE students
+(
+    id              INT, 
+    first_name      VARCHAR(50) NOT NULL,
+    last_name       VARCHAR(50) NOT NULL,
+    user_name       VARCHAR(50) NOT NULL,
+    student_email   VARCHAR(100)    NOT NULL,
+    scores          INT,
+    serial_number   INT NOT NULL,
+    created         BOOLEAN
+)ENGINE=INNODB;
 
 # CHANGE id to be primary and add auto increment:
-## CODE HERE
+ALTER TABLE students
+    MODIFY id INT(20) NOT NULL AUTO_INCREMENT,
+    ADD PRIMARY KEY(id);
 
 # Add a new column "phone_number" AFTER user_name column
-## CODE HERE
+ALTER TABLE students
+    ADD phone_number VARCHAR(15) AFTER user_name;
 
 # modify "student_email" column to be "email"
-## CODE HERE
+ALTER TABLE students
+    CHANGE COLUMN student_email email VARCHAR(100) NOT NULL;
 
 # modify "scores" column to be tinyint(4) , not accept any null values and set a default value to 0
-## CODE HERE
+ALTER TABLE students
+    MODIFY scores TINYINT(4) NOT NULL DEFAULT 0;
 
 # remove "first_name" and "last_name" columns from students table
-## CODE HERE
+ALTER TABLE students
+    DROP COLUMN first_name,
+    DROP COLUMN last_name;
 
 # ADD comment to phone_number column ?
-## CODE HERE
+ALTER TABLE students
+    MODIFY phone_number  VARCHAR(15) NOT NULL COMMENT 'this is phone number';
 
 # add created_at and updated_at column with timestamp and make update_at auto updated  ?
-## CODE HERE
+ALTER TABLE students
+    ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
