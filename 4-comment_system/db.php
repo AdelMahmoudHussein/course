1c468051007775d436711f26f44aed3b0cc16ba2
AdelMahmoudHussein <adelmahmoud1991@gmail.com>
2020-03-11 05:15:30 +0200

    edit connection check

--- 4-comment_system/db.php
+++ 4-comment_system/db.php
@@ -3,14 +3,15 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "123456789";
 $database = "demo";
 
 //Connection to database
 $db = new mysqli($servername, $username, $password, $database);
 
-//Returns true if there is NO connection
-if(!$db){
+//Returns true if there is NO connection 
+//if(!$db){         not work as $db is always an array so !$db is always false 
+if($db->connect_errno){
 	//Error Message
-	die("Connection failed: " . $db->connect_error);
+    die("Connection failed: " . $db->connect_error);
 }
