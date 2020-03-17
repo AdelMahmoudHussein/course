910b6207a1017e952dcff0025f9f5d264c271b52
AdelMahmoudHussein <adelmahmoud1991@gmail.com>
2020-03-17 18:05:33 +0200

    make a copy of 6-contact-us project for me

---
+++ 6-contact_us_me/quiz.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ahmed.Fakhr
+ * Date: 2019-08-27
+ * Time: 4:47 PM
+ */
+
+# Step 1 : implement validation message under inputs and filter spaces with php
+# Step 2 : add new input with age and use preg_match to match age
+# Step 3 : add new upload field and send contact form with this attachment to the email
+# Step 4 : make priority dropdown loaded dynamically from a priority table
+# Step 5 : Save all results in contact_us table and create page to see list of messages (you can use comment system)
+
+
+# google it
+// add a client side validation to validate all inputs not empty before submit.
+// Send inputs into HTML template to the  email via PHP use keyword (sending html email with php)
